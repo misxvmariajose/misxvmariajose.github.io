e6df138f29a446565161e0aa49cead7d0643050b
deleitempanadas <deleite.empanadas2012@gmail.com>
2022-07-19 14:15:28 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 window.sr = ScrollReveal();
 
 	
-	sr.reveal('.menu', {
+	sr.reveal('.nombres', {
 		duration: 1500, //tiempo
+		delay:1500,
 		origin: 'bottom', //origen
 		distance: '-50px' //distancia
 
@@ -10,16 +11,51 @@ window.sr = ScrollReveal();
 	}); 
 
 
-	sr.reveal('.titulo', {
+	sr.reveal('.nuestraBoda', {
 		duration: 1500, //tiempo
+		delay:1500,
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+sr.reveal('.nosCasamos', {
+		duration: 1500, //tiempo
+		delay:1500,
 		origin: 'top', //origen
 		distance: '-100px', //distancia
 		reset:1000
 
 	});
 
+sr.reveal('.dondeyCuando', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset:1000
 
+	});
+sr.reveal('.Tituloasistencia', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset:1000
 
+	});
+sr.reveal('.Tituloasistenciaespero', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+sr.reveal('.textoSeccion3', {
+		duration: 1500, //tiempo
+		origin: 'left', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
 	sr.reveal('.piepagina', {
 		duration: 1500, //tiempo
 		origin: 'bottom', //origen
