0c91d9fb6e80e4ec49a360e81df48dcf44fd84f1
deleitempanadas <deleite.empanadas2012@gmail.com>
2023-02-25 00:26:20 -0600

    first commit

--- js/countdown.js
+++ js/countdown.js
@@ -1,16 +1,16 @@
 simplyCountdown('#cuenta', {
-	year: 2022, // required
+	year: 2023, // required
 	month: 12, // required
-	day: 27, // required
+	day: 16, // required
 	hours: 10, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
-		days: 'Día',
-		hours: 'Hora',
-		minutes: 'Minuto',
-		seconds: 'Segundo',
-		pluralLetter: 's'
+		days: 'Días',
+		hours: 'Horas',
+		minutes: 'Min',
+		seconds: 'Seg',
+		pluralLetter: ''
 	},
 	plural: true, //use plurals
 	inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
